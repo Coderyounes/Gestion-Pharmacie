d5806c91dbb86d6deb1c2d4a97d17f4d46a43fbd
Coderyounes <younesanas2021@gmail.com>
2024-08-11 16:30:22 +0100

    Create the Welcome Function

--- auxllaryFeat.c
+++ auxllaryFeat.c
@@ -1,2 +1,18 @@
 #include "main.h"
 
+
+
+void Welcome(void) {
+    char *symbol = "*******";
+    printf("%s Bienvenue!%s\n\n%sGestion Des Produits%s\n", symbol, symbol, symbol, symbol);
+    printf("\nChoisie Votre Operation par numbre: \n"
+           "0: Exit le Program\n"
+           "1: Ajouter Un Produit\n"
+           "2: Ajouter des Produits\n"
+           "3: Lister tous les Produits\n"
+           "4: Supprimer un Produit\n"
+           "5: Operation de Recherche\n"
+           "6: Alimenter le Stock\n"
+           "7: Acheter Un Produit\n"
+           "8: Lister les Statistique de vente\n");
+}
